6f35522901c25eb45533fbc39000a51b71dd025f
Kaunda Kabwe <kkabwe@bluecodeltd.com>
2018-02-05 17:47:33 +0200

    added data_collector_app, edited gitignore file tpp

---
+++ data_collector_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DataCollectorAppConfig(AppConfig):
+    name = 'data_collector_app'
---
+++ data_collector_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+#               REPORT DETAIL FORM
+# *************************************************
+
+
+# HIV ACTIVITIES ORGANIZATION PARTICIPATES IN FORM
+# *************************************************
+
+
+#               STAKEHOLDER DIRECTORY
+# *************************************************
