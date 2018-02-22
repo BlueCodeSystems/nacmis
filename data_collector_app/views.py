8aee589a2973599817d16c2929e7ec4dcc443933
Kaunda Kabwe <kkabwe@bluecodeltd.com>
2018-02-22 16:04:02 +0200

    edited views and default settings folders

--- data_collector_app/views.py
+++ data_collector_app/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, welcome to the nacmis index page")
+
+def  stake_holder(request)
+    return HttpResponse()
+    
+def report(request):
+    return HttpResponse("Reporting form page.")
+
+def participation(request):
+    return HttpResponse("HIV activities organization participates in page.")
+
+def situation_room(request):
+    return HttpResponse("Reporting form page.")
