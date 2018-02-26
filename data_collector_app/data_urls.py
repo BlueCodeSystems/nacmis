1ec5736688d15a2ac45f473be0d59f09a60997ca
Kaunda Kabwe <kkabwe@bluecodeltd.com>
2018-02-26 09:12:42 +0200

    second commit of the day

--- data_collector_app/data_urls.py
+++ data_collector_app/data_urls.py
@@ -4,6 +4,9 @@ from . import views
 
 app_name = 'data_collector_app'
 
+from django.urls import path
+from . import views
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
 ]
