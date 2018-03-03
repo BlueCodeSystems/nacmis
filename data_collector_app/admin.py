bb1a4a02fc32f30969ccb1b4dd176a42ef10439b
Kaunda Kabwe <kkabwe@bluecodeltd.com>
2018-03-03 20:57:02 +0200

    edit of repository from council to NACMIS

--- data_collector_app/admin.py
+++ data_collector_app/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from .models import GeographicActivities
+from .models import ContactDetails
 
 # Register your models here.
 
 class ReportDetailAdmin(admin.ModelAdmin):
     fields = ['report_date', 'quarter', 'name', 'organization_type', 'location']
 
-admin.site.register(GeographicActivities)
+admin.site.register(ContactDetails)
 # admin.site.register(ReportDetail, ReportDetailAdmin)
