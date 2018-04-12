407498a2c7e2d6f14ae41d6c8ebd82c9368da1a4
Kaunda Kabwe - Work PC <kkabwe@bluecodeltd.com>
2018-04-12 18:13:29 +0200

    added stake_holder fieldset and adjusting code

--- data_entry/admin.py
+++ data_entry/admin.py
@@ -178,7 +178,7 @@ class ActivityReportFormAdmin(admin.ModelAdmin):
 
     fieldsets = (
         ('', {
-            'fields':('report_date', 'quarter_been_reported_on', 'name', 'telephone_number', 'email_address'),
+            'fields':('stake_holder', 'report_date', 'quarter_been_reported_on', 'name', 'telephone_number', 'email_address'),
         }),
         ('What types of care and support does your organization provide? (select all that apply)', {
             'fields': ('food_and_nutrition', 'shelter_and_care', 'protection_and_legal_aid', 'healthcare', 
