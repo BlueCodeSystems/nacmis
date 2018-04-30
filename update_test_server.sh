05355f8a787e338825f1307add6055b445993c20
Kaunda Kabwe - Work PC <kkabwe@bluecodeltd.com>
2018-04-30 13:44:15 +0200

    removed collect staic line in script

--- update_test_server.sh
+++ update_test_server.sh
@@ -4,6 +4,5 @@ cd /home/ubuntu/nacmis
 git checkout master
 git pull origin
 /home/ubuntu/.virtualenvs/nacmis/bin/python3 manage.py makemigrations --noinput
-/home/ubuntu/.virtualenvs/nacmis/bin/python3 manage.py collectstatic
 /home/ubuntu/.virtualenvs/nacmis/bin/python3  manage.py migrate
 sudo service apache2 restart
