eb4d9c803f5689a4df4b1e89215420f7611b3b5f
Kaunda Kabwe - Work PC <kkabwe@bluecodeltd.com>
2018-06-05 16:28:37 +0200

    issue #137: comming soon page complete with a count down clock.

--- data_entry/static/data_entry/js/comingsoon.js
+++ data_entry/static/data_entry/js/comingsoon.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2019 15:37:25").getTime();
+var countDownDate = new Date("Jul 5, 2018 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
