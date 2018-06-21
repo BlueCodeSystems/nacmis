95aeb103106226bb9bd31c405ae6a3e2ead1f173
Andre Lesa <alesa@bluecodeltd.com>
2018-06-21 13:44:12 +0000

    Fixed migrations.

--- update_test_server.sh
+++ update_test_server.sh
@@ -3,7 +3,4 @@
 cd /home/ubuntu/nacmis
 git checkout master
 git pull origin
-pip freeze > requirements.txt
-/home/ubuntu/.virtualenvs/nacmis/bin/python3 manage.py makemigrations --noinput
-/home/ubuntu/.virtualenvs/nacmis/bin/python3  manage.py migrate
 sudo service apache2 restart
