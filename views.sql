efa6fd59887d192701875c4e51a458b4cef4dbd0
Hans <zephyrfalcon@gmail.com>
2018-06-27 16:07:21 -0400

    added more views

--- views.sql
+++ views.sql
@@ -30,3 +30,76 @@ create or replace view vw_activityreportform as
   left outer join vw_stakeholderdirectory st
     on st.id = arf.stake_holder_name_id;
 
+-- join child tables with vw_activityreportform
+
+create or replace view vw_condomprogramming as
+  select ac.*,
+    co.id as condomprogramming_id,
+    co.condom_dist_point_num
+  from data_entry_condomprogramming co
+  left outer join vw_activityreportform ac
+    on ac.activityreportform_id = co.activity_form_id;
+
+create or replace view vw_condomprogramming2 as
+  select ac.*,
+    co.id as condomprogramming2_id,
+    co.female_condom_distributed_num,
+    co.male_condom_distributed_num
+  from data_entry_condomprogramming2 co
+  left outer join vw_activityreportform ac
+    on ac.activityreportform_id = co.activity_form_id;
+
+create or replace view vw_dacavalidation as
+  select ac.*,
+    dv.id as dacavalidation_id,
+    dv.validation_date as dacavalidation_date,
+    dv.form_validated,
+    dv.validated_by_id
+  from data_entry_dacavalidation dv
+  left outer join vw_activityreportform ac
+    on ac.activityreportform_id = dv.activity_form_id;
+
+create or replace view vw_experiencedphysicalviolence as
+  select ac.*,
+    ex.id as experiencedphysicalviolence_id,
+    ex.physical_female_less_10,
+    ex.physical_female_10_14,
+    ex.physical_female_15_19,
+    ex.physical_female_20_24,
+    ex.physical_female_25_plus,
+    ex.physical_male_less_10,
+    ex.physical_male_10_14,
+    ex.physical_male_15_19,
+    ex.physical_male_20_24,
+    ex.physical_male_25_plus
+  from data_entry_experiencedphysicalviolence ex
+  left outer join vw_activityreportform ac
+    on ac.activityreportform_id = ex.activity_form_id;
+    
+create or replace view vw_experiencedsexualviolence as
+  select ac.*,
+    ex.id as experiencedsexualviolence_id,
+    ex.sexual_female_less_10,
+    ex.sexual_female_10_14,
+    ex.sexual_female_15_19,
+    ex.sexual_female_20_24,
+    ex.sexual_female_25_plus,
+    ex.sexual_male_less_10,
+    ex.sexual_male_10_14,
+    ex.sexual_male_15_19,
+    ex.sexual_male_20_24,
+    ex.sexual_male_25_plus
+  from data_entry_experiencedsexualviolence ex
+  left outer join vw_activityreportform ac
+    on ac.activityreportform_id = ex.activity_form_id;
+
+create or replace view vw_iecmaterial as
+  select ac.*,
+    im.id as iecmaterial_id,
+    im.material_type as iecmaterial_type,
+    im.number_distributed as iecmaterial_number_distributed,
+    im.localized as iecmaterial_localized
+  from data_entry_iecmaterial im
+  left outer join vw_activityreportform ac
+    on ac.activityreportform_id = im.activity_form_id;
+
