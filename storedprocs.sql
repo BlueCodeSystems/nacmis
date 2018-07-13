64f3e2132e7b1a1397b6ae35dc1fa9ddd319de8d
Hans <zephyrfalcon@gmail.com>
2018-07-12 23:12:41 -0400

    finish up new stored proc

--- storedprocs.sql
+++ storedprocs.sql
@@ -1,5 +1,7 @@
--- 
 
+-- pick apart the data_entry_sexworker table, i.e. split the data up by age
+-- group and sex, and present these as fields in a different table, so we can
+-- filter by them.
 create or replace function sp_sex_workers_by_age_and_sex()
 returns table (
     indicator_name text, 
@@ -29,8 +31,27 @@ begin
             ('sex_workers', '10_14', 'female', row.sex_workers_female_10_14, 
              row.activity_form_id),
             ('sex_workers', '15_19', 'female', row.sex_workers_female_15_19, 
-             row.activity_form_id)
-            ;
+             row.activity_form_id),
+            ('sex_workers', '20_24', 'female', row.sex_workers_female_20_24, 
+             row.activity_form_id),
+            ('sex_workers', '25_29', 'female', row.sex_workers_female_25_29, 
+             row.activity_form_id),
+            ('sex_workers', '30_34', 'female', row.sex_workers_female_30_34, 
+             row.activity_form_id),
+            ('sex_workers', '35_plus', 'female', row.sex_workers_female_35_plus, 
+             row.activity_form_id),
+            ('sex_workers', '10_14', 'male', row.sex_workers_male_10_14, 
+             row.activity_form_id),
+            ('sex_workers', '15_19', 'male', row.sex_workers_male_15_19, 
+             row.activity_form_id),
+            ('sex_workers', '20_24', 'male', row.sex_workers_male_20_24, 
+             row.activity_form_id),
+            ('sex_workers', '25_29', 'male', row.sex_workers_male_25_29, 
+             row.activity_form_id),
+            ('sex_workers', '30_34', 'male', row.sex_workers_male_30_34, 
+             row.activity_form_id),
+            ('sex_workers', '35_plus', 'male', row.sex_workers_male_35_plus, 
+             row.activity_form_id);
     end loop;
 
     return query
