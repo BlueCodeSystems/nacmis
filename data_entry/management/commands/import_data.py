6aedfd26c20e1f392de0435d21552ecf27bcad5c
Hans <zephyrfalcon@gmail.com>
2018-08-02 21:38:26 -0400

    read login/password from credentials file

--- data_entry/management/commands/import_data.py
+++ data_entry/management/commands/import_data.py
@@ -1,6 +1,7 @@
 #!python
 
 import json
+import os
 
 from django.core.management.base import BaseCommand, CommandError
 #from polls.models import ...
@@ -10,14 +11,22 @@ import requests
 LOGIN_URL = "https://play.dhis2.org/2.30/dhis-web-commons/security/login.action"
 API_URL = "https://play.dhis2.org/demo/api/dataValueSets.json"
 
-# test credentials -- put real site credentials somewhere else!!
-USERNAME = "admin"
-PASSWORD = "district"
+def get_credentials():
+    whereami = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(whereami, "credentials.txt")
+    if os.path.exists(path):
+        with open(path) as f:
+            login = f.readline().strip()
+            password = f.readline().strip()
+            return (login, password)
+    else:
+        raise OSError("File does not exist: %s" % path)
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        login, password = get_credentials()
         sess = requests.Session()
-        r = sess.post(LOGIN_URL, auth=(USERNAME, PASSWORD))
+        r = sess.post(LOGIN_URL, auth=(login, password))
         print(r.status_code)
         print(r.headers)
         print(r.encoding)
@@ -35,3 +44,4 @@ class Command(BaseCommand):
             print(r2.encoding)
             j = r2.json()
             print(json.dumps(j, sort_keys=True, indent=4))
+
