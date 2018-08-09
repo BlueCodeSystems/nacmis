ce067c3d2f954969d63b4d2d59731caa446e30f1
Kaunda Kabwe - Work PC <kkabwe@bluecodeltd.com>
2018-08-09 12:21:29 +0200

    issue #320: DACA validation status toggles DACA initials on SARF

--- data_entry/static/admin/js/script.js
+++ data_entry/static/admin/js/script.js
@@ -3,12 +3,14 @@ var dacaValidationStatus = document.getElementById('id_dacavalidation_set-0-vali
 var hideMe = document.getElementById('id_dacavalidation_set-0-daca_initials');
 
 dacaValidationStatus.onchange = function name(event) {
-    var showOptions = ['approve','submitted','needs review']
+    var showOptions = ['approved','submitted','needs_review']
     var selectedOption = event.target['value'];
 
     if (showOptions.indexOf(selectedOption) >= 0){
        // console.log('event object from dacaValidationStatus',event.target['value']);
         hideMe.classList.add("show_acknowledgement");
+    } else {
+        hideMe.classList.remove("show_acknowledgement");
     }
 
     
