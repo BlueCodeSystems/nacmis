f1f27493c1b8929ec2bbcac0b4951b05001fa15b
Mellorison <mellorison@gmail.com>
2018-11-19 16:13:59 +0200

    fresh start

--- data_entry/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DataEntryConfig(AppConfig):
-    name = 'data_entry'
-    verbose_name = 'Data Entry'
--- update_test_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /home/ubuntu/nacmis
-git checkout master
-git pull origin
-sudo service apache2 restart
