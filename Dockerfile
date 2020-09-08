48369ce6087df9c0ab573ebd0077ee87f627a334
Andre Lesa <alesa@bluecodeltd.com>
2020-09-08 16:53:46 +0200

    NACMIS Configuration working with Superset on 09/08/20

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,11 @@ FROM python:3.7-buster
 
 ENV PYTHONUNBUFFERED 1
 RUN apt-get update && apt-get install -y gcc python3-dev libsasl2-dev libffi-dev libpq-dev git && rm -rf /var/lib/apt/lists/*
-RUN mkdir /src
-WORKDIR /src
-COPY . nacmis/
 WORKDIR /src/nacmis
-ENV PYTHONPATH="/:$PYTHONPATH"
+COPY requirements.txt /src/nacmis
 RUN pip install --no-cache-dir -r requirements.txt
+COPY . /src/nacmis/
+ENV PYTHONPATH="/:$PYTHONPATH"
 ENV DJANGO_SETTINGS_MODULE=nacmis_online.settings
 
 EXPOSE 8000
--- nacmis_online/settings_default.py
+++ nacmis_online/settings_default.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'nacmis_online.urls'
@@ -126,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_ROOT = os.getenv('DJANGO_STATIC_DIR', '')
+STATIC_ROOT = os.getenv('DJANGO_STATIC_DIR', 'static')
 
 STATIC_URL = '/static/'
 from .settings_default import *
